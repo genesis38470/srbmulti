fefd31c64024a81f43e4b68fc582d2d5fe6ece1f
genesis38470 <54453377+genesis38470@users.noreply.github.com>
2022-08-12 22:22:37 +0200

    Add files via upload

---
+++ start-mining-dagger.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+#This is an example you can edit and use
+#There are numerous parameters you can set, please check Help and Examples folder
+
+./SRBMiner-MULTI --disable-gpu --algorithm xdag --pool 86.48.0.150:13656!86.48.0.150:13656 --wallet gKun7kghiexz2k1j3Wdgg6RRcSsDdmO4 --worker srbminer
